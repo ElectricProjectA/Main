b83a11224a29715da5e0a187cb8fafaa841e89d0
sejoon <parksejoon313@gmail.com>
2022-11-19 16:16:54 +0900

    멤버 아이디등록

--- src/CurrentTime.java
+++ src/CurrentTime.java
@@ -54,41 +54,21 @@ public class CurrentTime {
             int twodotNum = date_time.length() - date_time.replace(":","").length();
             String[] input = date_time.split("-|/|:");
 
-
-            // date_time 0 붙는지 안붙는지 확인후 붙이기 - 테스트용.
-            /*
-            for(int i=0; i<input.length; i++) {
-                String stringStartsWithZero = "0";
-                if(input[i].length() < 2) {
-                    stringStartsWithZero = stringStartsWithZero + input[i];
-                    System.out.println("temp: "+stringStartsWithZero);
-                    input[i] = stringStartsWithZero;
-                }
-            }
-            // date_timeforComparison 형식을 2022-9-6/8:3 에서 2022-09-06/08:03으로 바꿔줌
-            String date_timeforComparison = "";
-            date_timeforComparison = input[0]+"-"+input[1]+"-"+input[2]+"/"+input[3]+":"+input[4];
-            // 검사
-            System.out.println("Test:"+date_timeforComparison);
-            */
-
-            /* date_time 형식수정 확인용 test
-            for(int i=0; i<input.length; i++) {
-                System.out.println(input[i]);
-            }
-            */
-
             boolean loop=false;
-            outerloop:
+            outerLoop:
             for(int i=0; i<input.length; i++){
                 input[i]=input[i].replaceFirst("^0+(?!$)", "");
                 for(int j=0; j<input[i].length(); j++){
-                    if((int)input[i].charAt(j) == 46)
-                        continue;
+                    if(input[i].charAt(j) == '.')
+                    {
+                        System.out.println("잘못된 입력입니다. 다시 입력해주세요.");
+                        loop=true;
+                        break outerLoop;
+                    }
                     else if((int)input[i].charAt(j) <48 || (int)input[i].charAt(j) >57){
                         System.out.println("잘못된 입력입니다. 다시 입력해주세요.");
                         loop=true;
-                        break outerloop;
+                        break outerLoop;
                     }
                 }
             }
@@ -249,9 +229,9 @@ public class CurrentTime {
                                         if((Integer.parseInt(split2[4]) < Integer.parseInt(split1[4]))) {
                                             System.out.println("기록된 time 보다 과거입니다");
                                             flag = false;
-                                        } else if ((Integer.parseInt(split2[4]) >= Integer.parseInt(split1[4]))) {
-                                            // 기록시간 이후의 입력 따라서 flag = true
-                                            // 분까지는 이후가 아니고 같다고 된다고 해놨음.
+                                        } else if ((Integer.parseInt(split2[4]) == Integer.parseInt(split1[4]))) {
+                                            System.out.println("기록된 time 보다 과거입니다");
+                                            flag = false;
                                         }
                                     }
                                 }
